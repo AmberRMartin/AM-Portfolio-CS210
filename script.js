79ca14ed902559b21f453cf29828b360ba828a69
AmberRMartin <armartin3@nic.edu>
2025-11-30 14:39:30 -0800

    Second little guy

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-const img = document.getElementById('images'); //get image
+const pencil = document.getElementById('pencil'); //get image
 let toggle = true;
 
-img.addEventListener('click', function(){
+pencil.addEventListener('click', function(){
     toggle = !toggle;
     if(toggle)
     {
-        img.src = 'images/decor/pencil_1.png';
+        pencil.src = 'images/decor/pencil_1.png';
     }else{
-        img.src = 'images/decor/pencil_2.png';
+        pencil.src = 'images/decor/pencil_2.png';
+    }
+
+});
+
+const lay = document.getElementById('lay'); //get image
+let toggle1 = true;
+
+lay.addEventListener('click', function(){
+    toggle1 = !toggle1;
+    if(toggle1)
+    {
+        lay.src = 'images/decor/lay_1.png';
+    }else{
+        lay.src = 'images/decor/lay_2.png';
     }
 
 });
